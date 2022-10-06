b3c152a47f195a7edcaf16cabf47e3a6c8a77907
Edgar Velazquez <90603328+WeroVlz@users.noreply.github.com>
2022-10-05 21:15:50 -0500

    Add files via upload

--- Practica2y3/Practica2y3/ViewControllerCrearCuenta.swift
+++ Practica2y3/Practica2y3/ViewControllerCrearCuenta.swift
@@ -7,9 +7,20 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseDatabase
 
 class ViewControllerCrearCuenta: UIViewController {
 
+    @IBOutlet weak var correo: UITextField!
+    @IBOutlet weak var contra: UITextField!
+    @IBOutlet weak var nombre: UITextField!
+    @IBOutlet weak var apellido: UITextField!
+    @IBOutlet weak var carrera: UITextField!
+    @IBOutlet weak var semestre: UITextField!
+    
+    let ref = Database.database().reference()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,14 +29,23 @@ class ViewControllerCrearCuenta: UIViewController {
     
 
     @IBAction func crearCuenta(_ sender: Any) {
-        Auth.auth().createUser(withEmail: "edgar@gmail.com", password: "123456")
+        Auth.auth().createUser(withEmail: correo.text ?? "", password: contra.text ?? "")
         {(AuthDataResult, Error) in
             if Error == nil
             {
-                let exito = UIAlertController(title: "Exito", message: "Usuario registrado", preferredStyle: .alert)
-                exito.addAction(UIAlertAction(title: "Aceptar", style: .default, handler: nil))
                 
-                self.present(exito, animated: true, completion: nil)
+                let usuario = AuthDataResult?.user
+                
+                self.ref.child("usuarios").child(usuario!.uid).setValue(["nombre": self.nombre.text ?? "", "apellido": self.apellido.text ?? "", "carrera": self.carrera.text ?? "", "semestre": self.semestre.text ?? ""]) { Error, DatabaseReference in
+                    if Error == nil
+                    {
+                        let exito = UIAlertController(title: "Exito", message: "Usuario registrado", preferredStyle: .alert)
+                        exito.addAction(UIAlertAction(title: "Aceptar", style: .default, handler: nil))
+                        
+                        self.present(exito, animated: true, completion: nil)
+                    }
+                }
+                
             }
             else{
                
